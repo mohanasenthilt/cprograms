f14c058bc2eb0f33f0939aff37e7af705c8a635b
mohanasenthilt <48051813+mohanasenthilt@users.noreply.github.com>
2019-02-27 16:18:31 +0530

    Create set1_4.c

---
+++ set1_4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+    char n;
+ printf("enter n");
+    scanf("%c",&n);
+    if(n>='a'&&n<='z')
+    {
+        printf("alphabets");
+    }
+    else
+    {
+        printf("not alphabets");
+    }
+    
+    
+    return 0;
+}
