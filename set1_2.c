1184b4a8f91519feb659239b8d77a176daaff67d
mohanasenthilt <48051813+mohanasenthilt@users.noreply.github.com>
2019-02-27 15:54:35 +0530

    Create set1_2.c

---
+++ set1_2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("enter the n");
+    scanf("%d",&n);
+    if(n%2==0)
+    {
+        printf("even");
+    }
+    else 
+    {
+        printf("odd");
+    }
+    
+    return 0;
+}
