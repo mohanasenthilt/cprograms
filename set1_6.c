4c8cf439e1320d32cc21bda2097f2fab787658b7
mohanasenthilt <48051813+mohanasenthilt@users.noreply.github.com>
2019-02-27 16:31:42 +0530

    Create set1_6.c

---
+++ set1_6.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+    int n;
+    printf("enter n");
+    scanf("%d",&n);
+   if(n%4==0)
+   {
+       printf("leap year");
+   }
+   else 
+   {
+       printf("not a leap year");
+   }
+  
+    
+    
+    return 0;
+}
