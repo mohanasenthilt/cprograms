60a0f8f090cf2a083002869f85020eb9de2fff0a
mohanasenthilt <48051813+mohanasenthilt@users.noreply.github.com>
2019-03-01 15:45:05 +0530

    Rename set1_7.c to set1_8.c

---
+++ set1_8.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,sum=0,n,k;
+    printf("enter n");
+    scanf("%d",&n);
+    for(i=0;i<=n;i++)
+    {
+        sum=sum+i;
+    }
+     printf("%d",sum); 
+    return 0;
+}
