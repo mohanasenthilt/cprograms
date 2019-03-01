4b1df98ff884002cb4c9d95d5e3d0c9c4921fa52
mohanasenthilt <48051813+mohanasenthilt@users.noreply.github.com>
2019-03-01 15:50:01 +0530

    Create set1_9.c

---
+++ set1_9.c
@@ -0,0 +1,11 @@
+# include<stdio.h>
+int main()
+{
+    int n,i,sum=0;
+    printf("enter");
+    scanf("%d",&n);
+    for(i=0;i<=n;i++)
+    {sum=sum+i;}printf("sum=%d",sum);
+    return 0;
+    
+}
