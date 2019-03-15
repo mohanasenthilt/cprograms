de9826c49cd0506dc64a0dfa64e5251c8f9181a8
mohanasenthilt <48051813+mohanasenthilt@users.noreply.github.com>
2019-03-15 16:15:48 +0530

    Rename pset1_6.c to pset1_6_2.c

---
+++ pset1_6_2.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    char a[10],b[10];
+    int i,j,m,n,f=0;
+    printf("enter 2 strings:");
+    scanf("%s %s",&a,&b);
+    m=strlen(a);
+    n=strlen(b);
+    if(m==n)
+    {
+        for(i=0;i<m;i++)
+        {
+            for(j=0;j<n;j++)
+            {
+                if(a[i]==b[j])
+                {
+                    f=1;
+                }
+            }
+        }
+    }
+    if(f==0)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    return 0;
+}
